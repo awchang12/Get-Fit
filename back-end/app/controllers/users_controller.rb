15c3d84153168c31208805d5c051133a4c197f68
github username <github email address>
2018-08-28 15:13:20 -0400

    added username to User table

--- back-end/app/controllers/users_controller.rb
+++ back-end/app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+
+    before_action :find_user, only: [:show, :update]
+
+    def index
+      @users = User.all
+      render json: @users, include: ['logs', 'goals', 'foods']
+  
+    end
+  
+    def show
+      render json: @user, include: ['logs', 'goals', 'foods']
+    end
+  
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: :accepted
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+      end
+    end
+  
+    def update
+  
+  
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:username, :id, :name, :height, :age)
+  
+    end
+  
+    def find_user
+      @user = User.find(params[:id])
+  
+    end
 end
--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-anthony = User.create(first_name: "Anthony", last_name: "Chang", weight: 210, height: 70, age: 26, gender: "male")
-jeff = User.create(first_name: "Jeff", last_name: "Johnson", weight: 160, height: 68, age: 35, gender: "male")
+anthony = User.create(first_name: "Anthony", last_name: "Chang", username: "awchang", weight: 210, height: 70, age: 26, gender: "male")
+jeff = User.create(first_name: "Jeff", last_name: "Johnson", username: "jj", weight: 160, height: 68, age: 35, gender: "male")
 
-alog1 = Log.create(date: "8/31/2018", weight: 208, user_id: 1)
+alog1 = Log.create(date: "2018-8-31", weight: 208, user_id: 1)
 agoal1 = Goal.create(content: "This week I want to go to the gym 3 times", user_id: 1)
